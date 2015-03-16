5d2cf346b020f00eeebca8dd88a2c03435be82f6
Johannes <vainjo4 t gmail.com>
2015-03-16 17:32:55 +0200

    added hello world

--- bufferAPI/bufferAPI/urls.py
+++ bufferAPI/bufferAPI/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'bufferAPI.views.home', name='home'),
+	
+    url(r'^$', 'bufferAPI.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
