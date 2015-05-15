4f6b33b0a36a71293d1c8fbd1810e3a801e9e9e3
Johannes <vainjo4 t gmail.com>
2015-05-15 10:13:54 +0300

    crude demo frontend

---
+++ demo/back.js
@@ -0,0 +1,30 @@
+﻿console.log("back.js");
+
+var activeSubpage = 1;
+
+function changeSubpage(srcnum, tgtnum){
+    
+    $("#page-"+srcnum).hide();
+    
+    if(tgtnum <= 4){
+      
+      $("#page-"+tgtnum).show();        
+      
+      if(tgtnum !== 1){
+
+      }
+    }
+    else throw new Error("NO PAGE AAARGAAHHH");
+};
+
+$(document).ready(function(){
+  $(".subpage-submit").on("click", function(){
+    
+    var srcpage = $(this).attr("sourcepage");
+    var targetpage = $(this).attr("targetpage");        
+    changeSubpage(srcpage, targetpage);
+    
+    //console.log("clicked:");
+    //console.log(this);
+  });
+});
--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){  
+﻿$(document).ready(function(){  
   var prods = 6;
   var box = $("#productbox");  
   $(".product").css("display","normal");
