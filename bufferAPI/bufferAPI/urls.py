a7c762d4d81a7c5baf774d0b6e69cf35dc2ce2df
aleksipyykkonen <aleksi.pyykkonen@aalto.fi>
2015-05-20 13:21:07 +0300

    Add views for customers and categories

--- bufferAPI/bufferAPI/urls.py
+++ bufferAPI/bufferAPI/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-	
-  url(r'^$', 'bufferapp.views.index'),	
-  url(r'^back$', 'bufferapp.views.back'),	
-  
-  url(r'^test$', 'bufferapp.views.test'),	
-  url(r'^products$', 'bufferapp.views.products'),	
- 
-  url(r'^bufferapp/', include('bufferapp.urls')),
-  url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', 'bufferapp.views.index'),
+    url(r'^back$', 'bufferapp.views.back'),
+
+    url(r'^test$', 'bufferapp.views.test'),
+    url(r'^products$', 'bufferapp.views.products'),
+    url(r'^categories$', 'bufferapp.views.catgories'),
+    url(r'^customers$', 'bufferapp.views.customers'),
+
+    url(r'^bufferapp/', include('bufferapp.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    )
--- bufferAPI/bufferapp/views.py
+++ bufferAPI/bufferapp/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Product
+from .models import Product, Category, Customer
+
 
 # test page
 def test(request):
-  return HttpResponse("Yay, test page")
+    return HttpResponse("Yay, test page")
+
 
 # front page
 def index(request):
-  return render(request, 'index.html')  
-  
+    return render(request, 'index.html')
+
+
 # "backend" page
 def back(request):
-  return render(request, 'back.html')
-    
+    return render(request, 'back.html')
+
+
 def products(request):
-  context = {"product_list" : Product.objects.all()}
-  return render(request, 'product.html', context)
+    context = {"product_list": Product.objects.all()}
+    return render(request, 'product.html', context)
+
+
+def catgories(request):
+    context = {"category_list": Category.objects.all()}
+    return render(request, 'category.html', context)
+
+
+def users(request):
+    context = {"customer_list": Customer.objects.all()}
+    return render(request, 'user.html', context)
