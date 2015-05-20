6ad1db642708e8501e4de03b195bdf61c0cdefd7
Johannes <vainjo4 t gmail.com>
2015-05-20 16:18:57 +0300

    basket item removal works

--- bufferAPI/bufferapp/static/basket.js
+++ bufferAPI/bufferapp/static/basket.js
@@ -1,17 +1,18 @@
 ﻿var productBasket = [];
+var listItemClass = "product-dropdown-list-item";
 
 function getProductDropdownElement(productInfo){
     var listelem = $("<li/>");
     var listlink = $("<a/>");
     var listremove = $("<span/>");
     
+    listelem.addClass(listItemClass);
     listelem.attr("data-product-id",productInfo.id);
     
     listlink.attr("href","#");
     listlink.text(""+productInfo.name+", "+productInfo.price + " e");
     listlink.addClass("basket-product-info")
-    
-    
+        
     listremove.text("X");
     listremove.addClass("basket-product-remover")
     listremove.addClass("btn")
@@ -23,24 +24,58 @@ function getProductDropdownElement(productInfo){
     return listelem;
 }
 
-function getEmptyDropdownElement(id){
+function getEmptyDropdownElement(cls){
     var listelem = $("<li/>");
     var listlink = $("<a/>");
-    listlink.attr("id","empty-basket-list-element");
+    listelem.addClass(cls);
     listlink.attr("href","#");
     listlink.text("Ostoskori on tyhjä");
     $(listelem).append(listlink);
     return listelem;
 }
 
-function dealWithBasketEmptiness(){
-  var id = "empty-basket-list-element";
-  var idSelector = "#"+id;
+function updateBasket(){
+  var prodSum = 0;
+  for(var i = 0; i < productBasket.length; i++){
+    prodSum += productBasket[i].price;
+  };    
+  $("#basket-prods").text(productBasket.length);
+  $("#basket-sum").text(prodSum.toFixed(2));    
 
-  if(!productBasket.length)
-    $("#basket-dropdown .basket-list").append(getEmptyDropdownElement(id));    
-  else if($(idSelector).length)
-    $(idSelector).remove();
+  // deal with empty basket  
+  var cls = "empty-basket-list-element";
+  var selector = "."+cls;
+
+  if($(selector).length)
+    $(selector).remove();
+  
+  if(!productBasket.length)    
+    $("#basket-dropdown .basket-list").append(getEmptyDropdownElement(cls));  
+}
+
+function productRemoveButtonFunction(elem){
+  //console.log("REMOVING")
+
+  var listParent = $(elem).parents("."+listItemClass);  
+  productBasket.splice($(listParent).index(), 1);
+  listParent.remove();
+  
+  /*
+  var keepGoing = true;  
+  for(var i = 0; i < productBasket.length; i++){
+    if(productBasket[i].id === targetId){
+      console.log("BEFORE-AFTER");
+      console.log()
+      console.log(productBasket.length);
+      productBasket.splice(i, 1);
+      console.log(productBasket.length);
+      listParent.remove();
+      //console.log("DID IT");      
+      keepGoing = false;   
+    }
+  } 
+  */  
+  updateBasket();
 }
 
 function productAddButtonFunction(buttonelem){    
@@ -55,32 +90,24 @@ function productAddButtonFunction(buttonelem){
     price : parseFloat(prodPrice),
     name : prodName
   };
-    
-  /*
-  console.log(prodId);
-  console.log(prodPrice);
-  console.log(productInfo);
-  */
+
   productBasket[productBasket.length] = productInfo;
-    
-  /*
-  console.log(this)
-  console.log(prodId)    */
-  console.log(productBasket);
+
+  console.log(productBasket);  
   
-    
-  var prodSum = 0;
-  for(var i = 0; i < productBasket.length; i++){
-    prodSum += productBasket[i].price;
-  };    
-  $("#basket-prods").text(productBasket.length);
-  $("#basket-sum").text(prodSum.toFixed(2));    
-  $("#basket-dropdown .basket-list").append(getProductDropdownElement(productInfo));  
-  dealWithBasketEmptiness();
+  var newElem = getProductDropdownElement(productInfo);
+  
+  $("#basket-dropdown .basket-list").append(newElem);
+  
+  $($(newElem).find(".basket-product-remover")).click(function(){
+    productRemoveButtonFunction(this);
+  });
+  
+  updateBasket();  
 }
 
 $(document).ready(function(){     
-  dealWithBasketEmptiness();
+  updateBasket();
   
   $(".add-product-to-basket").on("click", function(){    
     productAddButtonFunction(this);    
