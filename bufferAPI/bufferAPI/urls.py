8c656659c99eb293807fb429142b6641da2b9572
aleksipyykkonen <aleksi.pyykkonen@aalto.fi>
2015-05-20 18:40:50 +0300

    Show images in products

--- bufferAPI/bufferAPI/urls.py
+++ bufferAPI/bufferAPI/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,11 +12,10 @@ urlpatterns = patterns('',
     url(r'^products$', 'bufferapp.views.products'),
     url(r'^products/(?P<product_id>[0-9]+)/$', 'bufferapp.views.product'),
 
-
     url(r'^categories$', 'bufferapp.views.catgories'),
 
     url(r'^customers$', 'bufferapp.views.customers'),
 
     url(r'^bufferapp/', include('bufferapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    )
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bufferAPI/bufferapp/urls.py
+++ bufferAPI/bufferapp/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,7 +8,8 @@ urlpatterns = [
     # /bufferapp
     url(r'^$', views.index, name='index'),
     # url(r'^products/(?P<product_id>[0-9]+)/$', views.products, name='products'),
-
+    # (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    #     'document_root': settings.MEDIA_ROOT}),
     # ex: /bufferapp/products/5/
     # url(r'^/products/(?P<product_id>[0-9]+)/$', views.products, name='products'),
 ]
